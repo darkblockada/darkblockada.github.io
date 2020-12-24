394f14e363173cf6ff2c00bd034c53467ac02808
AlexsLoL <74259449+AlexsLoL@users.noreply.github.com>
2020-12-24 18:34:31 +0600

    new commit

---
+++ scripts/Script.js
@@ -0,0 +1,17 @@
+alert("BETA версия сайта на Блокаду");
+
+    document.ondragstart = noselect;
+    // запрет на перетаскивание
+    document.onselectstart = noselect;
+    // запрет на выделение элементов страницы
+    document.oncontextmenu = noselect;
+    // запрет на выведение контекстного меню
+    function noselect() {return false;}
+
+
+
+
+
+
+
+
